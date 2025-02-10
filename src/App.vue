6fad4205c828c958a0289c4fa85a8f816012057b
pere verges <pere.verges@jediupc.com>
2025-02-09 18:57:12 -0800

    primera prova

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,14 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import apunts_figura from '/isabelboncompte/src/assets/obra/apunts_figura.json'
+import Gallery from './components/Gallery.vue'
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-  </main>
+  <div>
+    <Gallery :images="apunts_figura" />
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
---
+++ src/components/Gallery.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="gallery">
+      <div v-for="(image, index) in images" :key="index" class="gallery-item">
+        <img :src="image.image" :alt="image.name" />
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      images: {
+        type: Array,
+        required: true,
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+.gallery {
+  column-count: 4; /* adjust the number of columns as needed */
+  column-gap: 8px; /* adjust the gap as needed */
+  row-gap: 10px;
+}
+
+.gallery-item {
+  display: inline-block;
+  width: 100%;
+}
+
+.gallery-item img {
+  width: 100%;
+  height: auto;
+}
+  </style>
