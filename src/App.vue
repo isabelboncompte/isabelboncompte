510a14d20cf51462fd2412a2d2f498fe950850a4
pere verges <pere.verges@jediupc.com>
2025-02-14 21:32:38 -0800

    jsons

--- src/App.vue
+++ src/App.vue
@@ -8,13 +8,15 @@ import els_llibres_i_la_rosa from './assets/obra/els_llibres_i_la_rosa.json'
 import espriu from './assets/obra/espriu.json'
 import gravat from './assets/obra/gravat.json'
 import homenatge_a_chantal_maillard from './assets/obra/homenatge_a_chantal_maillard.json'
+import miscellania from './assets/obra/miscellania.json'
+import murs_i_camins from './assets/obra/murs_i_camins.json'
 
 import Gallery from './components/Gallery.vue'
 </script>
 
 <template>
   <div>
-    <Gallery :images="homenatge_a_chantal_maillard" />
+    <Gallery :images="murs_i_camins" />
   </div>
 </template>
 
