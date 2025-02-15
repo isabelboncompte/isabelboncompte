e9fef6a5cf7a274dac8998901cad03699e66faac
pere verges <pere.verges@jediupc.com>
2025-02-14 20:43:06 -0800

    jsons

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,19 @@
 import apunts_figura from './assets/obra/apunts_figura.json'
 import apunts_paisatge from './assets/obra/apunts_paisatge.json'
 import botanica from './assets/obra/botanica.json'
+import dibuix from './assets/obra/dibuix.json'
+import dones_avançant from './assets/obra/dones_avançant.json'
+import els_llibres_i_la_rosa from './assets/obra/els_llibres_i_la_rosa.json'
+import espriu from './assets/obra/espriu.json'
+import gravat from './assets/obra/gravat.json'
+import homenatge_a_chantal_maillard from './assets/obra/homenatge_a_chantal_maillard.json'
 
 import Gallery from './components/Gallery.vue'
 </script>
 
 <template>
   <div>
-    <Gallery :images="botanica" />
+    <Gallery :images="homenatge_a_chantal_maillard" />
   </div>
 </template>
 
